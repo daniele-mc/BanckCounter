68ff455c2040f498f39a5233943da74670354a5c
Daniele Carvalho <44905383+daniele-mc@users.noreply.github.com>
2019-10-09 20:37:13 -0300

    Update Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -1,25 +1,28 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
         ContaBancaria contacorrente = new ContaCorrente();
         ContaBancaria containvestimento = new ContaInvestimento();
-        /*conta.setSaldo(212.15);
-        conta.calcularSaldo();
-        System.out.println("saldo: " + conta.getSaldo());*/
-        int opcao1;
+
+        int opcao1 =1;
         int opcao2;
-        String opcao3;
-        double dinheiro;
-        String resposta;
+ 
+        double dinheiro = 0.0;
         Scanner sc = new Scanner(System.in);
-        do {
-            System.out.println("Digite 1 p/ depositar um valor na conta");
+        sc.useLocale(Locale.US);
+        
+        while ((opcao1 >= 1)&&(opcao1 <=4)){
+
+            System.out.println("\nDigite 1 p/ depositar um valor na conta");
             System.out.println("Digite 2 p/ sacar um valor na conta");
             System.out.println("Digite 3 p/ transferir um valor na conta");
             System.out.println("Digite 4 p/ ver saldo final");
             opcao1 = (sc.nextInt());
+            
             switch (opcao1) {
+
                 case 1:
                     System.out.println("Digite 1 p/ fazer depósito na Conta Corrente");
                     System.out.println("Digite 2 p/ fazer depósito na Conta Investimento");
@@ -28,7 +31,8 @@ public class Principal {
                         System.out.println("Valor p/ depósito: ");
                         dinheiro = sc.nextDouble();
                         contacorrente.depositar(dinheiro);
-                    } else if (opcao2 == 2) {
+                    }
+                    else if (opcao2 == 2) {
                         System.out.println("Valor p/ depósito: ");
                         dinheiro = sc.nextDouble();
                         containvestimento.depositar(dinheiro);
@@ -37,17 +41,17 @@ public class Principal {
                     }
                     break;
 
-                /*case 2:
+                case 2:
                     System.out.println("Digite 1 p/ fazer saque na Conta Corrente");
                     System.out.println("Digite 2 p/ fazer saque na Conta Investimento");
                     opcao2 = (sc.nextInt());
                     if (opcao2 == 1) {
                         System.out.println("Valor p/ sacar: ");
-                        dinheiro = Double.parseDouble(sc.nextLine());
+                        dinheiro = sc.nextDouble();
                         contacorrente.sacar(dinheiro);
                     } else if (opcao2 == 2) {
                         System.out.println("Valor p/ sacar: ");
-                        dinheiro = Double.parseDouble(sc.nextLine());
+                        dinheiro = sc.nextDouble();
                         containvestimento.sacar(dinheiro);
                     } else {
                         break;
@@ -60,11 +64,11 @@ public class Principal {
                     opcao2 = (sc.nextInt());
                     if (opcao2 == 1) {
                         System.out.println("Valor p/ transferir: ");
-                        dinheiro = Double.parseDouble(sc.nextLine());
+                        dinheiro = sc.nextDouble();
                         contacorrente.transferir(containvestimento, dinheiro);
                     } else if (opcao2 == 2) {
                         System.out.println("Valor p/ transferir: ");
-                        dinheiro = Double.parseDouble(sc.nextLine());
+                        dinheiro = sc.nextDouble();
                         containvestimento.transferir(contacorrente, dinheiro);
 
                     } else {
@@ -76,15 +80,11 @@ public class Principal {
                     System.out.println("Conta corrente: " + contacorrente.getSaldo());
                     System.out.println("Conta investimento: " + containvestimento.getSaldo());
                     break;
-*/
-                default:
+                    default:
                     System.out.println("Opção invalida");
                     break;
             }
-
-            System.out.println("Deseja fazer outra operação? (s/n)");
-            resposta = sc.nextLine();
-
-        } while (resposta == "n");
+        }
     }
 }
+
